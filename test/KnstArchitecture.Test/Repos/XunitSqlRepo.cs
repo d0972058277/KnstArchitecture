ae9128d9f77a74760603b59623c842dc8536aa29
PorriDG3 <gn02053231@gmail.com>
2020-07-06 14:55:18 +0800

    XunitSqlRepo

---
+++ test/KnstArchitecture.Test/Repos/XunitSqlRepo.cs
@@ -0,0 +1,30 @@
+using KnstArchitecture.Test.Abstraction;
+using KnstArchitecture.UnitOfWorks;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace KnstArchitecture.Test.Repos
+{
+    public class XunitSqlRepo : XunitKnstArchSql
+    {
+        [Fact]
+        public void Connection()
+        {
+            var uow = ServiceProvider.GetRequiredService<ISqlUnitOfWork>();
+            var repo = ServiceProvider.GetRequiredService<ITestSqlRepo>();
+            var defaultSession = uow.GetDefaultDbSession();
+
+            Assert.Equal(defaultSession.GetConnection(), repo.Connection);
+        }
+
+        [Fact]
+        public void Transaction()
+        {
+            var uow = ServiceProvider.GetRequiredService<ISqlUnitOfWork>();
+            var repo = ServiceProvider.GetRequiredService<ITestSqlRepo>();
+            var defaultSession = uow.GetDefaultDbSession();
+
+            Assert.Equal(defaultSession.GetTransaction(), repo.Transaction);
+        }
+    }
+}
