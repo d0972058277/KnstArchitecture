6b17d847f58a086587e8bef64873bab0dab20692
御霖 <gn02053231@gmail.com>
2020-07-07 22:54:40 +0800

    update servicecollection extensions

--- src/KnstArchitecture/ServiceCollectionExtension.cs
+++ src/KnstArchitecture/ServiceCollectionExtension.cs
@@ -31,10 +31,12 @@ namespace Microsoft.Extensions.DependencyInjection
             var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.DefinedTypes.Where(x => x.GetInterfaces().Contains(typeof(T)) && !x.IsInterface && !x.IsAbstract)).ToList();
             foreach (var type in types)
             {
+                services.Add(new ServiceDescriptor(type, type, lifetime));
+
                 var interfaces = type.GetInterfaces().ToList();
                 foreach (var @interface in interfaces)
                 {
-                    services.Add(new ServiceDescriptor(@interface, type, lifetime));
+                    services.Add(new ServiceDescriptor(@interface, sp => sp.GetRequiredService(type), lifetime));
                 }
 
             }
@@ -46,10 +48,12 @@ namespace Microsoft.Extensions.DependencyInjection
             var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.DefinedTypes.Where(x => x.GetInterfaces().Contains(typeof(T)) && !x.IsInterface && !x.IsAbstract)).ToList();
             foreach (var type in types)
             {
+                services.TryAdd(new ServiceDescriptor(type, type, lifetime));
+
                 var interfaces = type.GetInterfaces().ToList();
                 foreach (var @interface in interfaces)
                 {
-                    services.TryAdd(new ServiceDescriptor(@interface, type, lifetime));
+                    services.TryAdd(new ServiceDescriptor(@interface, sp => sp.GetRequiredService(type), lifetime));
                 }
             }
             return services;
